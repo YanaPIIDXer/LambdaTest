ea16db002ed2943b9711d6897d6dd813212997b3
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-07-05 23:34:09 +0900

    APIのテスト用ボタン ※POSTはまだAPIが無いので未実装

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <div id="app">
     <h1>Lambda Test</h1>
+    <button @click="getTest">GET</button><br />
+    <button @click="postTest">POST</button><br />
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'App',
   components: {
@@ -14,6 +18,25 @@ export default {
     }
   },
   methods: {
+    /**
+     * GETのテスト
+     */
+    getTest: async function () {
+      try {
+        const response = await axios.get("http://localhost:3000/hello")
+        if (!response || response.status !== 200) { throw response }
+        alert(response.data.message)
+      } catch (error) {
+        console.error(error)
+        alert("エラー")
+      }
+    },
+    /**
+     * POSTのテスト
+     */
+    postTest: async function () {
+
+    },
   },
 }
 </script>
